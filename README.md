a15f817be9659ee7f91fad43155256a673a44db4
KVarun97 <varun@humancloud.co.in>
2021-08-24 15:42:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-u83x2a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-u83x2a)
---
+++ index.js
@@ -0,0 +1,18 @@
+// Import stylesheets
+import './style.css';
+var data = [{
+  z: [[22, 0,0,0,0], [0, 33, 0, 0, 0], [0,0,44,0,0], [0,0,0,55,0], [0,0,0,0,66]],
+  x: [18, 19, 20, 25, 30],
+  y: [73, 76, 79, 87, 90],
+  type: 'contour'
+}];
+
+var layout = {
+  title: 'Setting the X and Y Coordinates in a Contour Plot'
+};
+
+Plotly.newPlot('myDiv', data, layout);
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
